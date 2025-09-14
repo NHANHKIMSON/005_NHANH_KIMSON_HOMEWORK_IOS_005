62ae4e13d0b6d46e3dbc30a6a3a5b78ce9e686e4
nhanhkimson.biu@gmail.com <nhanhkimson.biu@gmail.com>
2025-09-14 23:45:22 +0700

    Initial Commit

---
+++ Note APP/Note_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_APPApp.swift
+//  Note APP
+//
+//  Created by Apple on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Note_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Note APPTests/Note_APPTests.swift
@@ -0,0 +1,17 @@
+//
+//  Note_APPTests.swift
+//  Note APPTests
+//
+//  Created by Apple on 9/14/25.
+//
+
+import Testing
+@testable import Note_APP
+
+struct Note_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
